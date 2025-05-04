f956ba97f21036fcd225bd81ea1271cd3427c5fd
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2025-05-04 20:37:36 +0530

    changes added workflow

---
+++ agent/workflow.py
@@ -0,0 +1,10 @@
+class GraphBuilder:
+    def __init__(self):
+        self.graph = {}
+
+    def _chatbt_node(self, state:State):
+        pass
+    def build():
+        pass
+    def get_graph(self):
+        pass
